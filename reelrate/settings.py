336e472a1ad15a2b9464869a336bd01cfbb37d7b
Vaibhav <time4ever0@gmail.com>
2025-05-08 20:57:37 +0100

    error fixed

--- reelrate/settings.py
+++ reelrate/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['moviematch.up.railway.app', 'localhost']
+ALLOWED_HOSTS = ['web-production-7b557.up.railway.app', 'localhost']
 
 
 
